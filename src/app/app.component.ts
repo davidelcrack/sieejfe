35b07399478dcbbb0f5c126f4aab9915f6c44c6f
davidelcrack <ledavis275@gmail.com>
2018-04-21 20:17:22 -0500

    COMMIT: Ajustes de servidor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -78,7 +78,7 @@ export class AppComponent {
     //localStorage.setItem('USER', null);//this.currentUser = JSON.parse(localStorage.getItem('USER'))   
     this.currentUser = localStorage.getItem('USER');
     console.log(JSON.parse(localStorage.getItem('LOGGEADO')))  ;
-    this.estado=JSON.parse(localStorage.getItem('LOGGEADO')).estadoEnum; //no local
+    //this.estado=JSON.parse(localStorage.getItem('LOGGEADO')).estadoEnum; //no local
     //this.estado='NUEVO'; // sí local
     console.log(this.estado);
     console.log(this.currentUser);
